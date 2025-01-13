e0a94f85a31eb5e89e4c3b1a6f1921f4214a2879
Rahul Natha <67892338+hey-rahul@users.noreply.github.com>
2025-01-13 17:00:08 +0530

    Workout log viewer roll out

---
+++ assets/js/workout.js
@@ -0,0 +1,26 @@
+// Add event listeners to all workout-row elements
+document.querySelectorAll('.workout-row').forEach(row => {
+    row.addEventListener('click', (event) => {
+        // Prevent clicking on the 'view' div itself from triggering
+        if (event.target.classList.contains('workout-view')) return;
+
+        const workoutView = row.querySelector('.workout-view');
+        const expandIcon = row.querySelector('.workout-expand');
+
+        if (workoutView.style.display === 'none' || workoutView.style.display === '') {
+            workoutView.style.display = 'block';
+            expandIcon.innerHTML = '&#9650;'; // Up arrow
+        } else {
+            workoutView.style.display = 'none';
+            expandIcon.innerHTML = '&#9660;'; // Down arrow
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const workoutViewDivs = document.querySelectorAll(".workout-view");
+    
+    workoutViewDivs.forEach(div => {
+        div.innerHTML = div.innerHTML.replace(/;/g, ';<br>');
+    });
+});
